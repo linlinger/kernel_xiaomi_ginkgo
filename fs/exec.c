0fbebf5cff4a45724d76b8775258f7366ea8c2fe
Joel Gómez <nahuelgomez329@gmail.com>
2023-05-28 23:13:12 +0000

    fs: guard kernelsu-specific functions
    Fixes these linking errors:
    
    ld.lld: error: undefined symbol: ksu_handle_faccessat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(sys_faccessat)
    
    ld.lld: error: undefined symbol: ksu_vfs_read_hook
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(vfs_read)
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(vfs_read)
    
    ld.lld: error: undefined symbol: ksu_handle_vfs_read
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(vfs_read)
    
    ld.lld: error: undefined symbol: ksu_handle_stat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(vfs_statx)
    
    ld.lld: error: undefined symbol: ksu_execveat_hook
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(do_execveat_common)
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(do_execveat_common)
    
    ld.lld: error: undefined symbol: ksu_handle_execveat_sucompat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(do_execveat_common)
    
    ld.lld: error: undefined symbol: ksu_handle_execveat
    >>> referenced by ld-temp.o
    >>>               vmlinux.o:(do_execveat_common)
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>
    Signed-off-by: Akari <akariondev@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1715,13 +1715,17 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-// KernelSU hook
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
-
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
 /*
  * sys_execve() executes a new program.
  */
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1733,7 +1737,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-    ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
+	#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
